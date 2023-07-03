92cbba7d75a93e786f69b40b6d8d17b6c8224c57
Johnathon Melton <seventh.acoustic@gmail.com>
2023-07-03 09:53:03 -0500

    Static Exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write($"Fahrenheit -> Celsuis: {TempConverter.FahrenheitToCelsius(52)}");
+            Console.WriteLine();
+
+            Console.Write($"Celsuis -> Fahrenheit: {TempConverter.CelsiusToFahrenheit(27)}");
+            Console.WriteLine();
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            // C = (F - 32) * 5 / 9;
+            var answer = (fahrenheit - 32) * 5 / 9;
+            return answer;
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            var answer = (celsius * 9) / 5 + 32;
+            return answer;
+        }
+    }
+}
